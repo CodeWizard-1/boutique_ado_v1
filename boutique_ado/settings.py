01fe1ea76b1406c7dd380741b08171c984425de0
CodeWizard-1 Oleksandr Kravchenko <kravchenko.dp@gmail.com>
2023-11-30 11:54:11 +0000

    Add  allauth

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s92#6%uzgo2x+akwsg6=wi^7!qsm9)s7sha)235j@rqkk%8h1_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-codewizard1-boutiqueado-jfoguu7kunf.ws-eu106.gitpod.io']
 
 
 # Application definition
@@ -80,6 +80,16 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
